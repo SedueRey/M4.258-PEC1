7404cb5fbe247808f6be4070831f3155964e66e5
Eduardo Rey <eduardo.rey@ticarum.es>
2021-10-08 11:42:09 +0200

    Desktop y throttle del evento scroll

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,6 +11,25 @@
  */
 
 +( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
+  window.addEventListener('scroll', throttle(callback, 750), false);
+
+  function throttle(fn, wait) {
+    let time = Date.now();
+    return function() {
+      if ((time + wait - Date.now()) < 0) {
+        fn();
+        time = Date.now();
+      }
+    }
+  }
+
+  function callback() {
+    console.log('callback');
+    const header = document.getElementById('document-mainnav');
+    if (window.pageYOffset > 100 ){
+      header.classList.add('mainnav--is-fixed');
+    } else {
+      header.classList.remove('mainnav--is-fixed');
+    }
+  }
 } )();
